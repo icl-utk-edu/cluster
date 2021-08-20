d56e6a8501707a7e4476648637a72415180c02e8
root <root@sorbitol.icl.utk.edu>
2021-08-20 00:53:09 -0400

    cleanup

--- ansible/roles/headnode2/files/dnsmasq/run.sh
+++ ansible/roles/headnode2/files/dnsmasq/run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash -e
 
+CONF=/opt/cluster/config
+
 NAME=dnsmasq
 cd /opt/$NAME
 
 docker stop $NAME || true
-docker rm $NAME   || true
 
 if [[ $1 == 'rebuild' ]]; then
    docker build -t $NAME .
 fi
 
-docker run --name $NAME -d --restart=unless-stopped \
+NODES=$CONF/dnsmasq-nodes.conf
+touch $NODES
+
+docker run --name $NAME -d --rm --restart=unless-stopped \
    --net host \
-   -v /opt/$NAME/dnsmasq.conf:/etc/dnsmasq.conf \
-   -v /opt/$NAME/nodes.conf:/etc/dnsmasq.d/nodes.conf \
+   -v $NODES:/etc/dnsmasq.d/nodes.conf \
+   -v $CONF/pxe:/pxe \
    --privileged \
    $NAME > /dev/null 2>&1
 
 echo Done
-
-#   -p 53:53/udp \
-#   -p 67:67/udp \
-#   -p 69:69/udp \
