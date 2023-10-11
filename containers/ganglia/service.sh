592f3a7805444a5697b2826cc567f2de1916640a
Gerald <gerald@ragghianti.com>
2023-10-11 14:38:06 -0400

    ganglia and rocm node updates

--- containers/ganglia/service.sh
+++ containers/ganglia/service.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+iptables -P INPUT DROP
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type any -j ACCEPT
+iptables -A INPUT -p tcp -s 160.36.131.0/24 --dport 80 -j ACCEPT
+
+
 mkdir -p /run/php-fpm
 /usr/sbin/php-fpm --nodaemonize &
 
