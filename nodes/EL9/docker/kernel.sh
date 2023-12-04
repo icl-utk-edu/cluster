3109a8854f41ae364ba9b90207829b725a5d7c7e
Gerald <gerald@ragghianti.com>
2023-12-04 10:29:00 -0500

    Creating new node images

---
+++ nodes/EL9/docker/kernel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+VARIANT=$1
+
+if [[ $VARIANT = 'kernel3' ]]; then
+   KVER=3.10.0-1160.102.1.el7.x86_64
+   source /tmp/spack/share/spack/setup-env.sh
+   spack load gcc
+   dnf --disablerepo=* --enablerepo=centos7,centos7-updates,epel7 install \
+   dkms kernel-$KVER kernel-devel-$KVER
+fi
+if [[ $VARIANT = 'kernel4' ]]; then
+   KVER=4.18.0-477.10.1.el8_8.x86_64
+   dnf -y --disablerepo=* --enablerepo=RL8,epel8 install \
+	   dkms kernel-$KVER kernel-core-$KVER kernel-devel-$KVER kernel-modules-$KVER
+fi
+if [[ $VARIANT = 'kernel66' ]]; then
+   KVER=6.6.1-1.el9.elrepo.x86_64
+   dnf -y --enablerepo=elrepo-kernel-archive install kernel-ml-$KVER kernel-ml-devel-$KVER
+fi
+if [[ $VARIANT = 'base' ]]; then
+   KVER=6.1.62-1.el9.elrepo.x86_64
+   dnf -y --enablerepo=elrepo-kernel-archive install kernel-lt-$KVER kernel-lt-devel-$KVER
+fi
+
+echo $KVER > /boot/kver
+
