7538e7972a9b0f5d313393779fc6000a196d4868
Gerald <gerald@ragghianti.com>
2024-05-15 14:00:14 -0400

    nagios updates

--- containers/nagios/entrypoint.sh
+++ containers/nagios/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-chmod g+w  /var/spool/nagios/cmd
-conf=/etc/nagios/nagios.cfg
-/usr/sbin/nagios -v $conf
-/usr/sbin/nagios -d $conf
-
 mkdir -p /run/php-fpm
 chown apache.apache /run/php-fpm
 sudo -u apache /usr/sbin/php-fpm
@@ -12,7 +7,8 @@ sudo -u apache /usr/sbin/php-fpm
 /usr/sbin/httpd
 #-DFOREGROUND
 
-export HOME=/tmp/
+crond
+
 chmod g+w  /var/spool/nagios/cmd
 conf=/etc/nagios/nagios.cfg
 /usr/sbin/nagios -v $conf
